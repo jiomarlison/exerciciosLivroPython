9e96087771f5bf55cdc6b8608ea14973cfbe4e70
Secretaria <jiomar213@gmail.com>
2023-04-03 16:17:58 -0300

    Programa 6.16

--- Cápitulo 6/Programas/Prog 6.16.py
+++ Cápitulo 6/Programas/Prog 6.16.py
@@ -1,3 +1,8 @@
+# LISTA COM ELEMENTOS DE TIPOS DIFERENTES
 produto1 = ['maça', 10, 0.30]
 produto2 = ['pera', 5, 0.75]
 produto3 = ['kiwi', 4, 0.98]
+
+print(produto1)
+print(produto2)
+print(produto3)
