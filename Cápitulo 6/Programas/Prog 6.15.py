e6d00700e1ae4b8fab3451232169f7dc746f959b
Secretaria <jiomar213@gmail.com>
2023-04-03 16:16:29 -0300

    UPD Programa 6.15

--- Cápitulo 6/Programas/Prog 6.15.py
+++ Cápitulo 6/Programas/Prog 6.15.py
@@ -1,3 +1,4 @@
+# IMPRESSÃO DE UMA LISTA DE STRING, LETRA A LETRA
 l = ['maças', 'peras', 'kiwis']
 for s in l:
     for letra in s:
---
+++ Cápitulo 6/Programas/Prog 6.16.py
@@ -0,0 +1,3 @@
+produto1 = ['maça', 10, 0.30]
+produto2 = ['pera', 5, 0.75]
+produto3 = ['kiwi', 4, 0.98]
